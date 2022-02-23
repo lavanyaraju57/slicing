4d845fe1000c1cc0cbafb79d8877e828cdb1e83c
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-02-23 13:11:19 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,69 @@
+# print(len("python"))
+
+# a="python"
+# print(a,type(a))
+# print(a[0])
+
+# a=None
+# print(a,type(a))
+
+# print("python"[0])
+# print("python"[1])
+# print("python"[2])
+# print("python"[3])
+# print("python"[4])
+# print("python"[5])
+# print("python"[6])
+
+# b='department'
+# print(b[0])
+# print(b[7])
+# print(b[2]+b[8])
+# print(b[3]+b[2]+b[3]+b[4]+b[5])
+# print(b[3]+b[2:6])   #shortcut
+
+# a='python'
+# print(a[:])
+# print(a[0:])
+# print(a[5:5])#blank line
+# print(a[1:])
+# print(a[4:])
+# print(a[6:])3blank line
+# print(a[:3])
+# print(a[1:5])
+# print(a[3:7])
+# print(a[7:3])#blank line
+
+# a="python"
+
+# print(a[5])
+# print(a[0:-1])
+# print(a[0:-2])
+# print(a[0:-3])
+# print(a[0:-4])
+# print(a[0:-6])
+
+# r="python"
+# print(r[2:5])
+# print(r[3:4])
+# print(r[5:2])
+# print(r[5:5])
+
+# a="python"
+# print(a[::])
+# print(a[1::])
+# print(a[5::])
+# print(a[41::])
+# print(a[::1])
+
+# print(a[0::2])
+# print(a[1:3:2])
+
+# a="python"
+# print(a[::-1])
+# print(a[::-2])
+# print(a[::-3])
+# print(a[::-4])
+# print(a[::-5])
+# print(a[::-6])
+
